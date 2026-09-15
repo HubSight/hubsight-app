515e8b6ff2193f18a77051e04bf44bfdbbc02dce
anhquoctran <aquoc.hue@outlook.com>
2026-09-15 08:47:03 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.hubsight.hubsight_app"
+    namespace = "com.quoctran.hubsight"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.hubsight.hubsight_app"
+        applicationId = "com.quoctran.hubsight"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/quoctran/hubsight/MainActivity.kt
+++ android/app/src/main/kotlin/com/quoctran/hubsight/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hubsight.hubsight_app
+package com.quoctran.hubsight
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
